4e0a143e689b452e0ffddd15f09951e1d41d28b9
Claude Hähni <chaehni@outlook.com>
2024-10-08 14:40:34 +0200

    add output boxes and build site

--- 04-dashboard-tips/app.py
+++ 04-dashboard-tips/app.py
@@ -7,11 +7,18 @@ from shinywidgets import render_plotly
 from shiny import reactive, render
 from shiny.express import input, ui
 
+import plotly.graph_objects as go
+
 ICONS = {
     "user": fa.icon_svg("user", "regular"),
     "wallet": fa.icon_svg("wallet"),
+    "briefcase": fa.icon_svg("briefcase"),
+    "piggy-bank": fa.icon_svg("piggy-bank"),
+    "stocks": fa.icon_svg("arrow-trend-up"),
     "currency-dollar": fa.icon_svg("dollar-sign"),
     "ellipsis": fa.icon_svg("ellipsis"),
+    "income": fa.icon_svg("hand-holding-dollar"),
+    "spending": fa.icon_svg("credit-card"),
 }
 
 # Add page title and sidebar
@@ -54,22 +61,27 @@ with ui.navset_pill_list(id="pill", widths=(2,10)):
             # ui.input_slider("interest", "Interest Rate", min=0, max=100, value=7, post="%")
             # ui.input_action_button("reset", "Reset filter", width="200px")
         
-        with ui.layout_columns(col_widths=[12, 12]):
-            with ui.card(full_screen=True):
-                with ui.card_header(class_="d-flex justify-content-between align-items-center"):
-                    "Income/Expense Distribution"
-
-                with ui.popover(title="Toggles"):
-                    ICONS["ellipsis"]
-                    ui.input_radio_buttons(  
-                        "radio_earnings_type",  
-                        "Select chart type",  
-                        {"1": "Sankey", "2": "Bar Chart"},  
-                    )
-                @render_plotly
-                def chart_render():
-                    import plotly.graph_objects as go
-                    if input.radio_earnings_type() == "1":
+        with ui.layout_columns(col_widths=(4, 4, 4)):
+            with ui.value_box(showcase=ICONS.get("income"), theme="bg-gradient-red-orange", fill=False, heigth="20px"):
+                "Monatliche Einkünfte"
+                @render.express
+                def render_income():
+                    f"{total_income():,} CHF"
+            with ui.value_box(showcase=ICONS.get("spending"), theme="bg-gradient-red-orange", fill=False):
+                "Monatliche Ausgaben"
+                @render.express
+                def render_expenses():
+                    f"{total_expenses():,} CHF"
+            with ui.value_box(showcase=ICONS.get("piggy-bank"), theme="bg-gradient-green-teal", fill=False):
+                "Ersparnisse"
+                @render.express
+                def render_savings():
+                    f"{total_income()-total_expenses():,} CHF"
+        with ui.layout_columns(col_widths=[6, 6]):
+            with ui.navset_card_underline(title="Einkünfte und Ausgaben"):
+                with ui.nav_panel("sankey"):
+                    @render_plotly
+                    def sankey_render():
                         node_colors = ["#1f77b4", "#4682b4", "#5f9ea0", "#6495ed", "#00bfff", "#87cefa", "#add8e6", "#b0e0e6", "green"]  # Various blue shades and green
                         fig = go.Figure(data=[go.Sankey(
                             node = dict(
@@ -83,13 +95,13 @@ with ui.navset_pill_list(id="pill", widths=(2,10)):
                             source = [0, 1, 2, 2, 2, 2, 2, 2], # indices correspond to labels
                             target = [2, 2, 3, 4, 5, 6, 7, 8],
                             value = [input.salary_input(),
-                                     input.dividend_input(),
-                                     input.living_cost_input(),
-                                     input.groceries_input(),
-                                     input.food_input(),
-                                     input.transport_cost_input(),
-                                     input.hobbys_input(),
-                                     input.salary_input() + input.dividend_input() - input.living_cost_input() - input.groceries_input() - input.food_input() - input.transport_cost_input() - input.hobbys_input(),
+                                    input.dividend_input(),
+                                    input.living_cost_input(),
+                                    input.groceries_input(),
+                                    input.food_input(),
+                                    input.transport_cost_input(),
+                                    input.hobbys_input(),
+                                    input.salary_input() + input.dividend_input() - input.living_cost_input() - input.groceries_input() - input.food_input() - input.transport_cost_input() - input.hobbys_input(),
                                     ]
                         ))])
 
@@ -101,30 +113,41 @@ with ui.navset_pill_list(id="pill", widths=(2,10)):
                         
                         fig._config = fig._config | {'displayModeBar': False}
                         return fig
-                    
-                    elif input.radio_earnings_type() == "2":
+                        
+                with ui.nav_panel("bars"):
+                    @render_plotly
+                    def bars_render():
                         # Sample data for income and spendings
-                        categories = ['Transport', 'Food', 'Rent', 'Entertainment', 'Utilities', 'Miscellaneous']
-                        incomes = [input.salary_input()]  # Total earnings for the period
-                        spendings = [-500, -700, -1200, -300, -400, -150]  # Example spendings for each category
+                        income_categories = ["Einkommen", "Gewinn"]
+                        income_amounts = [input.salary_input(), input.dividend_input()]
+                        spending_categories = ["Wohnen", "Haushalt", "Essen", "Transport", "Hobbys"]
+                        spending_amounts= [-input.living_cost_input(), -input.groceries_input(), -input.food_input(), -input.transport_cost_input(), -input.hobbys_input()]
 
                         # Create a bar chart
                         fig = go.Figure()
 
-                        # Add income bars (green)
+                        # Add income bars (blue)
                         fig.add_trace(go.Bar(
-                            x=['Income'],
-                            y=incomes,
-                            name='Income',
-                            marker_color='green'
+                            x= income_categories,
+                            y= income_amounts,
+                            name='Einkünfte',
+                            marker_color='blue'
                         ))
 
                         # Add spendings bars (red)
                         fig.add_trace(go.Bar(
-                            x=categories,
-                            y=spendings,
-                            name='Spendings',
+                            x=spending_categories,
+                            y=spending_amounts,
+                            name='Ausgaben',
                             marker_color='red'
+                        ))
+
+                        # Add savings bar (green)
+                        fig.add_trace(go.Bar(
+                            x=["Ersparnisse"],
+                            y=[input.salary_input() + input.dividend_input() - input.living_cost_input() - input.groceries_input() - input.food_input() - input.transport_cost_input() - input.hobbys_input()],
+                            name='Ersparnisse',
+                            marker_color='green'
                         ))
 
                         # Update layout
@@ -138,8 +161,6 @@ with ui.navset_pill_list(id="pill", widths=(2,10)):
                         )
                         fig._config = fig._config | {'displayModeBar': False}
                         return fig
-
-        
 
             with ui.card(full_screen=True):
                 with ui.card_header(class_="d-flex justify-content-between align-items-center"):
@@ -156,7 +177,7 @@ with ui.navset_pill_list(id="pill", widths=(2,10)):
 
                     # Parameters
                     initial_amount = 20000  # Initial investment
-                    years = np.arange(0, 30, 1)  # Time period in years
+                    years = np.arange(0, 60, 1)  # Time period in years
                     interest_rate = 0.07  # Interest rate from user input
                     inflation_rate = 0.02  # Inflation rate (2%)
 
@@ -204,7 +225,27 @@ with ui.navset_pill_list(id="pill", widths=(2,10)):
                     fig.update_layout(modebar_remove=['zoom', 'pan', 'toImage'])
                     return fig
     with ui.nav_panel("Immobilien"):
-        "Panel A content"
+        with ui.layout_columns(col_widths=(3, 3, 3, 3)):
+            with ui.value_box(showcase=ICONS.get("wallet"), theme="bg-gradient-indigo-purple", fill=False, heigth="20px"):
+                "Privatgelder"
+                @render.express
+                def render_private_assets():
+                    f"{input.bank_account_input():,} CHF"
+            with ui.value_box(showcase=ICONS.get("briefcase"), theme="bg-gradient-indigo-purple", fill=False):
+                "Säule 2"
+                @render.express
+                def render_pillar2():
+                    f"{input.pillar2_input():,} CHF"
+            with ui.value_box(showcase=ICONS.get("piggy-bank"), theme="bg-gradient-indigo-purple", fill=False):
+                "Säule 3a"
+                @render.express
+                def render_pillar3():
+                    f"{input.pillar3a_input():,} CHF"
+            with ui.value_box(showcase=ICONS.get("stocks"), theme="bg-gradient-indigo-purple", fill=False):
+                "Aktiendepots"
+                @render.express
+                def render_investments():
+                    f"{input.investment_input():,} CHF"
     with ui.nav_panel("Portfolio"):
         "Panel A content"
 
@@ -227,4 +268,10 @@ ui.include_css(app_dir / "styles.css")
 # Reactive calculations and effects
 # --------------------------------------------------------
 
+@reactive.calc
+def total_income():
+    return input.salary_input() + input.dividend_input()
 
+@reactive.calc
+def total_expenses():
+    return input.living_cost_input() + input.groceries_input() + input.food_input() + input.transport_cost_input() + input.hobbys_input()
